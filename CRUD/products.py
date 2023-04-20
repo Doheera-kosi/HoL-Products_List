07958dd901b3cab3816ba083df097e863a319350
Doheera-kosi <kupourevans99@gmail.com>
2023-04-20 20:41:25 +0000

    Add All products endpoint

--- CRUD/products.py
+++ CRUD/products.py
@@ -11,8 +11,13 @@ products = [
     {'id': 144, 'name': 'Black Marker', 'price': 1.99}
 ]
 
-#
-#Add all the REST API end-points here
-#
+
+# Add all the REST API end-points here
+
+# GET request - http://localhost:5000/products
+@app.route('/products', methods=['GET'])
+def get_products():
+    return jsonify(products)
+
 
 app.run(port=5000,debug=True)
